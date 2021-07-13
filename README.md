b97ae0ba7b92ccf605b55daf00816941419422b8
kburger <burger.github@gmail.com>
2021-07-13 12:28:19 +0200

    First test for FDP discovery.

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# ci
-CI workflows
+# TWOC CI
---
+++ ci-count-twoc-fdp.py
@@ -0,0 +1,28 @@
+"""
+Test for discovering TWOC FAIR Data Point instances.
+"""
+
+import requests
+import sys
+
+INDEX = "https://twoc-index.fair-dtls.surf-hosted.nl"
+ENTRIES_ENDPOINT = INDEX + "/index/entries/all"
+EXPECTED_ENTRIES = 3
+
+numEntries = 0
+
+for entry in requests.get(ENTRIES_ENDPOINT).json():
+    try:
+        clientUrl = entry["clientUrl"]
+        print(f"Requesting client url {clientUrl}")
+        requests.get(clientUrl)
+        print("Resolved client url")
+        numEntries += 1
+    except:
+        sys.exit(1)
+
+print(f"Found {numEntries} entries")
+
+if numEntries < EXPECTED_ENTRIES:
+    print(f"Expected {EXPECTED_ENTRIES} entries")
+    sys.exit(1)
